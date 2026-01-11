edc1f44201418c2a4428ee2b9b7fe31dc659b843
DanielMachluf <danielmachluf420@gmail.com>
2026-01-11 15:30:02 +0200

    added get price feautre

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 
-alert("Hello, world!");
+
+
+const vat = price * 1.18
+function getPrice() {
+    price = document.getElementById("getPrice").value;
+    alert("the price with vat is: " + vat);
+}
